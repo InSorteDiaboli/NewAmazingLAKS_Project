01728939662127eed6a34ce008368754cd69c01a
Louis Verond Høgholt <louisvhoegholt@gmail.com>
2017-11-30 12:58:31 +0100

    all pages + classes

---
+++ NewAmazingLAKS_Project/Model/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewAmazingLAKS_Project.Model
+{
+    class Customer
+    {
+    }
+}
