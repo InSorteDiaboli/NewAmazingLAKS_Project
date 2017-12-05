00213c5001ebc092f194444d0a79b661ddd65825
Louis Verond Høgholt <louisvhoegholt@gmail.com>
2017-12-05 10:29:14 +0100

    commit

--- NewAmazingLAKS_Project/AddCustomer.xaml.cs
+++ NewAmazingLAKS_Project/AddCustomer.xaml.cs
@@ -20,9 +20,9 @@ namespace NewAmazingLAKS_Project
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class AddProduct : Page
+    public sealed partial class AddCustomer : Page
     {
-        public AddProduct()
+        public AddCustomer()
         {
             this.InitializeComponent();
         }
